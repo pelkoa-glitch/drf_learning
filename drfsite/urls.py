7851f57d04f4861fdfff4377572d66ea4d091a0e
pelkoa-glitch <hvngmvnvibe@gmail.com>
2023-08-29 17:33:04 +0300

    Добавлены пути для работы с токенами

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from guitars.views import GuitarsAPIList, GuitarsAPIUpdate, GuitarsAPIDestroy
 
@@ -8,10 +9,15 @@ from guitars.views import GuitarsAPIList, GuitarsAPIUpdate, GuitarsAPIDestroy
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/drf-auth/', include('rest_framework.urls')),
     path('api/v1/guitars/', GuitarsAPIList.as_view()),
     path('api/v1/guitars/<int:pk>/', GuitarsAPIUpdate.as_view()),
     path('api/v1/guitarsdelete/<int:pk>/', GuitarsAPIDestroy.as_view()),
-
+    path('api/v1/auth/', include('djoser.urls')),
+    path(r'auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
 # Как работает роутер под капотом.
 # class MyCustomRouter(routers.SimpleRouter):
