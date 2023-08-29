ac0a349695b7ee327c3db6c3baea87f0eaca1198
pelkoa-glitch <hvngmvnvibe@gmail.com>
2023-08-29 15:42:10 +0300

    Прописаны пути под новые Api с учетом permissions

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -1,10 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path, include
-from guitars.views import GuitarsViewSet
-from rest_framework import routers
+from django.urls import path
 
+from guitars.views import GuitarsAPIList, GuitarsAPIUpdate, GuitarsAPIDestroy
 
+# from guitars.views import GuitarsViewSet # Импорт для использования ViewSet
+# from rest_framework import routers # Импорт для использования роутера
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/guitars/', GuitarsAPIList.as_view()),
+    path('api/v1/guitars/<int:pk>/', GuitarsAPIUpdate.as_view()),
+    path('api/v1/guitarsdelete/<int:pk>/', GuitarsAPIDestroy.as_view()),
+
+]
 # Как работает роутер под капотом.
 # class MyCustomRouter(routers.SimpleRouter):
 #     routes = [
@@ -23,15 +31,15 @@ from rest_framework import routers
 #     ]
 
 
-router = routers.DefaultRouter()
+# router = routers.DefaultRouter()
 # Если из ViewSet убираем атрибут queryset,
 # в роутере необходимо указать basename.
-router.register(r'guitars', GuitarsViewSet, basename='guitars')
+# router.register(r'guitars', GuitarsViewSet, basename='guitars')
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),  # http://127..0.0.1:8000/api/v1/guitars/
-    # path('api/v1/guitarslist/', GuitarsViewSet.as_view({'get': 'list'})),
-    # path('api/v1/guitarslist/<int:pk>/', GuitarsViewSet.as_view({'put': 'update'})),
-
-]
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('api/v1/', include(router.urls)),  # http://127..0.0.1:8000/api/v1/guitars/
+#     path('api/v1/guitarslist/', GuitarsViewSet.as_view({'get': 'list'})),
+#     path('api/v1/guitarslist/<int:pk>/', GuitarsViewSet.as_view({'put': 'update'})),
+#
+# ]
