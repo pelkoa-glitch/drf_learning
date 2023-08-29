ca53cbb4f199888c6b7f12b41be07c73c5556453
pelkoa-glitch <hvngmvnvibe@gmail.com>
2023-08-29 12:20:08 +0300

    Добавлен роутер

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -1,11 +1,16 @@
 
 from django.contrib import admin
-from django.urls import path
-from guitars.views import GuitarsAPIList, GuitarsAPIUpdate, GuitarsAPIDetailView
+from django.urls import path, include
+from guitars.views import GuitarsViewSet
+from rest_framework import routers
+
+router = routers.SimpleRouter()
+router.register(r'guitars', GuitarsViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/guitarslist/', GuitarsAPIList.as_view()),
-    path('api/v1/guitarslist/<int:pk>/', GuitarsAPIUpdate.as_view()),
-    path('api/v1/guitarsdetail/<int:pk>/', GuitarsAPIDetailView.as_view())
+    path('api/v1/', include(router.urls)),  # http://127..0.0.1:8000/api/v1/guitars/
+    # path('api/v1/guitarslist/', GuitarsViewSet.as_view({'get': 'list'})),
+    # path('api/v1/guitarslist/<int:pk>/', GuitarsViewSet.as_view({'put': 'update'})),
+
 ]
